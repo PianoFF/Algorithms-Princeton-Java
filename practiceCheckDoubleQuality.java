1a4fca5fe1f917752c05395422a736a231708a74
pianoFF <fangfei1star@gmail.com>
2020-11-23 20:38:57 -0500

    practice

---
+++ practiceCheckDoubleQuality.java
@@ -0,0 +1,16 @@
+import edu.princeton.cs.algs4.StdOut;
+
+public class practiceCheckDoubleQuality
+{
+  public static void main(String[] args)
+  {
+    double x;
+    double y;
+
+    x = 1.2342;
+    y = 0.33456;
+
+    if (x > 1 || y > 1) StdOut.println("flase");
+    else StdOut.println("true");
+  }
+}
